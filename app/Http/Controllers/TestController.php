e59088e82f694994c26103b55b99e9671a2e934c
weihung <drum831831@gmail.com>
2025-10-30 10:41:56 +0800

    測試api

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Test API is working',
+            'timestamp' => now()
+        ]);
+    }
+}
