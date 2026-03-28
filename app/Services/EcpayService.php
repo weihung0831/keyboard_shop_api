9057b2b873dd9e6425483eda27d620797506eff4
weihung0831 <drum831@gmail.com>
2026-03-29 02:36:11 +0800

    fix(payment): hide non-credit-card payment methods on ECPay

--- app/Services/EcpayService.php
+++ app/Services/EcpayService.php
@@ -63,6 +63,7 @@ class EcpayService
             'ItemName' => $this->buildItemName($order),
             'ReturnURL' => $this->return_url,
             'ChoosePayment' => 'Credit',
+            'IgnorePayment' => 'WebATM#ATM#CVS#BARCODE#ApplePay#TWQR',
             'EncryptType' => 1,
         ];
 
